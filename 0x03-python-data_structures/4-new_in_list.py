ecbb7f692c094095c9b637a3d2fc5a71777bc618
Godwindavid07 <davidgodwin065@gmail.com>
2024-03-18 19:25:59 -0700

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -9,6 +9,7 @@ def new_in_list(my_list, idx, element):
     list: A new list with the replacement, or a copy of the original list
     if the index is invalid.
     """
-    new_list = my_list.copy()
     if 0 <= idx < len(my_list):
-        new_list[idx] = element
+        return my_list
+    new_list[idx] = element
+    return new_list
