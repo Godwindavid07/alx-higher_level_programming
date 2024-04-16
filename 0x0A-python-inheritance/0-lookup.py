ef0b0c14f5d95c31a9697a847f307762b83a6f63
Godwindavid07 <davidgodwin065@gmail.com>
2024-04-15 18:58:54 -0700

    my commit

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,15 +2,4 @@
 
 def lookup(obj):
     """Returns a list of user-defined attributes and methods of an object."""
-    result = []
-    attrs = dir(obj)
-
-    builtin_attrs = set(dir(type(obj)))
-    for item in attrs:
-        if not item.startswith('__') and item not in builtin_attrs:
-            attribute = getattr(obj, item)
-            if callable(attribute):
-                result.append(item + '()')
-            else:
-                result.append(item)
-                return result
+    return dir(obj)
