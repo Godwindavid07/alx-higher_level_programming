c6adaa798bbadf9dc8df8e9e125c97e2d5c03f86
Godwindavid07 <davidgodwin065@gmail.com>
2024-04-15 18:23:56 -0700

    my commit

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,28 +1,16 @@
 #!/usr/bin/python3
+
 def lookup(obj):
-    """Returns a list of available attributes and methods of an object.
-
-    Args:
-        obj: The object to inspect.
-
-    Returns:
-        A list of strings representing the available attributes and methods.
-    """
-
+    """Returns a list of user-defined attributes and methods of an object."""
     result = []
-
-    # Get the object's attributes using dir()
     attrs = dir(obj)
 
-    # Iterate through attributes and filter them
+    builtin_attrs = set(dir(type(obj)))
     for item in attrs:
-        # Exclude private and magic methods/attributes
-        if not item.startswith('__'):
-            # Check if it's a method (callable)
-            attribute = getattr(obj, item)  # Get the actual attribute value
+        if not item.startswith('__') and item not in builtin_attrs:
+            attribute = getattr(obj, item)
             if callable(attribute):
-                result.append(item + '()')  # Indicate it's a method
+                result.append(item + '()')
             else:
                 result.append(item)
-    
-    return result
+                return result
