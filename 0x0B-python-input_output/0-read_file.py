de0c343f69b8af692dd8ff0881800986b0da8f36
Godwindavid07 <davidgodwin065@gmail.com>
2024-04-16 18:12:03 -0700

    my second commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,7 +5,7 @@
 def read_file(filename=""):
     """Reads utf-8 text file and print its content
     Args:
-    filename: the name of the file to read
+    filename (str): the name of the file to read
     """
     with open(filename, 'r', encoding='utf-8') as myfile:
         print(myfile.read(), end="")
---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""defines a func that writes to a file"""
+def write_file(filename="", text=""):
+    """write a string to txt file
+    Args:
+    filename (str): name if the file to write text
+    text (str): the text to write
+    Returns:
+    NO of characters written.
+    """
+    with open(filename, 'w', encodding="utf-8") as myfile:
+        return (myfile.write(text))
