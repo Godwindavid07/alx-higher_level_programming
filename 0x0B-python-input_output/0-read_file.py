5b90d324dbc889f6579f92d7dad2d75d2403948a
Godwindavid07 <davidgodwin065@gmail.com>
2024-04-16 17:45:23 -0700

    currection

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-
+""" defines file reading func """
 def read_file(filename=""):
     """Reads utf-8 text file and print its content
     Args:
     filename: the name of the file to read
     """
     with open(filename, 'r', encoding='utf-8') as myfile:
-        print(myfile.read())
+        print(myfile.read(), end="")
